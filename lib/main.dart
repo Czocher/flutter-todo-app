7ef5838e55cb80edc147ebfbe1b0d8f23c56fe72
Paweł Jan Czochański <czochanski@gmail.com>
2019-02-20 10:07:24 +0100

    Add more functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:uuid/uuid.dart';
 
 void main() => runApp(ScopedModel<TodoModel>(
     model: TodoModel(),
     child: MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MainPage(),
     )));
 
+class Todo {
+  Uuid uuid;
+  String title;
+  String content;
+  bool done;
+
+  Todo({@required this.title, this.content = "", this.done = false}) {
+    this.uuid = Uuid();
+  }
+}
+
 class TodoModel extends Model {
-  final _todos = <String>["aa", "Bbbbbbbbb"];
+  var _todos = <Todo>[
+    Todo(title: "Tytul", content: "tresc", done: true),
+    Todo(title: "Tytul2", content: "tresc2", done: false),
+  ];
 
-  List<String> get todos => _todos;
+  List<Todo> get todos => _todos;
 
-  void addTodo(String todo) {
+  void add(Todo todo) {
     _todos.add(todo);
+    notifyListeners();
+  }
+
+  void update(Todo todo) {
+    Todo found = this._todos.singleWhere((it) => it.uuid == todo.uuid);
+    found?.title = todo.title;
+    found?.content = todo.content;
+    found?.done = todo.done;
+    if (found != null) {
+      notifyListeners();
+    }
+  }
+
+  void remove(Todo todo) {
+    this._todos = this._todos.where((it) => it.uuid != todo.uuid).toList();
     notifyListeners();
   }
 }
@@ -42,31 +73,51 @@ class MainPage extends StatelessWidget {
 }
 
 class TodoList extends StatelessWidget {
-  List<String> _todos;
+  final List<Todo> _todos;
 
   TodoList(this._todos);
+
+  void _dismiss(BuildContext context, Todo todo) {
+    TodoModel model = ScopedModel.of<TodoModel>(context);
+    model.remove(todo);
+    Scaffold.of(context)
+        .showSnackBar(SnackBar(content: Text("${todo.title} removed")));
+  }
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemBuilder: (context, index) => TodoListElement(_todos[index]),
+      itemBuilder: (context, index) => Dismissible(
+            key: Key(_todos[index].uuid.toString()),
+            child: TodoListElement(_todos[index]),
+            onDismissed: (direction) => _dismiss(context, _todos[index]),
+          ),
       itemCount: _todos.length,
     );
   }
 }
 
 class TodoListElement extends StatelessWidget {
-  String _content;
+  final Todo _todo;
 
-  TodoListElement(this._content);
+  TodoListElement(this._todo);
+
+  void _toggle(BuildContext context) {
+    this._todo.done = !this._todo.done;
+    TodoModel model = ScopedModel.of<TodoModel>(context);
+    model.update(this._todo);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(this._content),
-      subtitle: Text("subtitle"),
-      leading: Icon(Icons.check),
-      trailing: Icon(Icons.arrow_forward),
+    return ExpansionTile(
+      title: Text(this._todo.title),
+      children: <Widget>[
+          Text(this._todo.content, textAlign: TextAlign.left),
+      ],
+      leading: this._todo.done == true
+          ? Icon(Icons.check_box)
+          : Icon(Icons.check_box_outline_blank),
     );
   }
 }
@@ -74,18 +125,11 @@ class TodoListElement extends StatelessWidget {
 class CreateTodoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final form = TodoForm();
     return Scaffold(
       appBar: AppBar(
         title: Text("Create new todo"),
-        actions: <Widget>[
-          IconButton(
-            icon: Icon(Icons.send),
-            onPressed: () {},
-          ),
-        ],
       ),
-      body: form,
+      body: TodoForm(),
     );
   }
 }
@@ -97,12 +141,15 @@ class TodoForm extends StatefulWidget {
 
 class _TodoFormState extends State<TodoForm> {
   final _formKey = GlobalKey<FormState>();
-  final _controller = TextEditingController();
+  final _titleController = TextEditingController();
+  final _contentController = TextEditingController();
 
-  void submitForm(context) {
+  void _submitForm(context) {
     if (_formKey.currentState.validate()) {
-      TodoModel m = ScopedModel.of<TodoModel>(context);
-      m.addTodo(_controller.value.text);
+      TodoModel model = ScopedModel.of<TodoModel>(context);
+      model.add(Todo(
+          title: _titleController.value.text,
+          content: _contentController.value.text));
       Navigator.pop(context);
     }
   }
@@ -111,24 +158,46 @@ class _TodoFormState extends State<TodoForm> {
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
-      child: Column(
-        children: <Widget>[
-          TextFormField(
-            controller: _controller,
-            decoration: InputDecoration(
-              hintText: "Enter your todo",
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(height: 24.0),
+            TextFormField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: "Title",
+                labelText: "Title",
+              ),
+              validator: (value) {
+                if (value.isEmpty) {
+                  return "Title cannot be empty";
+                }
+              },
             ),
-            validator: (value) {
-              if (value.isEmpty) {
-                return "Todo cannot be empty";
-              }
-            },
-          ),
-          RaisedButton(
-            onPressed: () => this.submitForm(context),
-            child: Text("Submit"),
-          ),
-        ],
+            const SizedBox(height: 24.0),
+            Expanded(
+              child: TextFormField(
+                controller: _contentController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Content",
+                  labelText: "Content",
+                ),
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            SizedBox(
+              width: double.infinity,
+              child: FlatButton(
+                color: Colors.blue,
+                onPressed: () => this._submitForm(context),
+                child: Text("Submit"),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
